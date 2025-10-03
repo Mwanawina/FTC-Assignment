233c335914cdf8f6b559ccf1df1b992e63effb37
SananaM <sananam@hyperiondev.com>
2025-10-03 17:02:15 +0200

    Added comments for the TODOs

--- packages/hardhat/contracts/LiquidityCustodian.sol
+++ packages/hardhat/contracts/LiquidityCustodian.sol
@@ -12,7 +12,10 @@ import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
 
 import "hardhat/console.sol";
 
+// This contract will act like a custodian for Uniswap liquidity NFTs.
+
 // COMPLETED TODO: Make this contract a IERC721Receiver
+// The contract must be able to receive Uniswap V3 Position NFTS which are the ERC721 tokens
 contract LiquidityCustodian {
     address public immutable token;
     address public immutable weth;
@@ -57,27 +60,35 @@ contract LiquidityCustodian {
     function createPool() public {
         // COMPLETED TODO: assign WETH and MC addresses to new variables token0_ and token1_ addresses
         // NOTE: for a uniswap pool token0 address should be strictly less than token1
+        // token0 and token1 must be given in dictionary order (alphabetical by address) becuase this is required by Uniswap
         (address token0_, address token1_) = token < weth ? (token, weth) : (weth, token);
 
         // COMPLETED TODO: create a liquidity pool of WETH and MC token using uniswap factory
+        // Actually create the pool using Uniswap's factory
         address liquidityPoolAddress_ = uniswapV3Factory.createPool(token0_, token1_, _dexPoolFee);
 
         // COMPLETED TODO: assign the liquidityPoolAddress
+        // The pool address created needs to be stored for future use
         liquidityPoolAddress = liquidityPoolAddress_;
 
         // COMPLETED TODO: get the square of the price using at the _poolTick TickMath library from Uniswap
         // NOTE: sqrtPriceX96 is a A Fixed point Q64.96 number representing the sqrt of the ratio of the two assets (token1/token0)
+        // Figure out the starting price in Uniswap’s math system (sqrt ratio format)
         uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(_poolTick);
 
         // COMPLETED TODO: initialize the pool with the sqrtPriceX96
+        // With the starting sqrtPrice, initialise the new pool
         IUniswapV3Pool(liquidityPoolAddress_).initialize(sqrtPriceX96);
 
         // COMPLETED TODO: emit the PoolCreate event
+        // Let the world know a pool was created
         emit PoolCreated(token, liquidityPoolAddress_, sqrtPriceX96);
     }
 
     // COMPLETED TODO: create the onERC721Received function. It will create a deposit in the custodian contract
     // NOTE: the function should create an NFT deposit for the sender of NFT to this smart contract
+    // Triggered when someone sends this contract a liquidity NFT (ERC721)
+    // saves details about the NFT for bookkeeping
     function onERC721Received(
         address operator,
         address from,
@@ -92,8 +103,12 @@ contract LiquidityCustodian {
     /// @notice Stores the details of the deposited liquidity position's NFT
     /// @param owner The address of the liquidity position owner
     /// @param tokenId Id of the ERC721 token minted to represent ownership of the liquidity position
+    
+    // An internal helper that stores info about a newly receibed liquidity NFT
     function _createDeposit(address owner, uint256 tokenId) internal {
         // COMPLETED TODO: get the details of the liquidity position from the uniswap NFT manager
+        // Ask Uniswap's position manager for details about the NFT
+        // All the commas and empty spaces mean "ignore this return value". The ones listed are the ones we need, Solidity won't let us skip without a comma
         (
             , 
             , 
@@ -111,6 +126,7 @@ contract LiquidityCustodian {
 
         // COMPLETED TODO: create Deposit object using the detail above and store it in the deposits mapping
         // NOTE: the owner of the deposit is whoever has created the liquidity position through this contract or elsewhere and sent this contract their NFT
+        // Save the details under this tokenId
         deposits[tokenId] = Deposit({
             owner: owner,
             liquidity: liquidity,
@@ -119,6 +135,7 @@ contract LiquidityCustodian {
         });
 
         // COMPLETED TODO: store the tokenId in the owners' token array that is in the liquidityTokens mapping
+        // Add the tokenId to the owner's list
         liquidityTokens[owner].push(tokenId);
     }
 
@@ -127,7 +144,7 @@ contract LiquidityCustodian {
     /// @param index The index of the tokenId to be returned if the owner has more than 1 liquidity positions
     /// @return uint256 TokenId that represents the liquidity position owned by the address
     
-    // !!!!! //////////// WHY WAS THIS EXCLUSED THOOOOUGH ///////////////////////
+    // Lets us look up one of our NFTs by index if we have multiple
     function liquidityTokenByIndex(address owner, uint256 index) public view virtual returns (uint256) {
         require(liquidityTokens[owner].length > 0, "The address has no liquidity tokens");
         
@@ -157,17 +174,22 @@ contract LiquidityCustodian {
         (uint256 amount0ToMint, uint256 amount1ToMint) = token < weth ? (tokenAmountToMint, wethAmountToMint) : (wethAmountToMint, tokenAmountToMint);
 
         // COMPLETED TODO: transfer the token0, amount0ToMint, from the msg.sender to this contract
+        // Move user's tokens into this contract
         TransferHelper.safeTransferFrom(token0_, msg.sender, address(this), amount0ToMint);
 
         // COMPLETED TODO: transfer the token1, amount1ToMint, from the msg.sender to this contract
+        // Same thing, move user's tokens into this contract
         TransferHelper.safeTransferFrom(token1_, msg.sender, address(this), amount1ToMint);
 
         // COMPLETED TODO: approve the uniswap position manager to transfer amount0ToMint of token0
+        // Approve Uniswap's position manager to use them
         TransferHelper.safeApprove(token0_, address(uniswapPositionManager), amount0ToMint);
         
         // COMPLETED TODO: approve the uniswap position manager to transfer amount1ToMint of token1
+        // Same thing, approve Uniswap's position manager to use them
         TransferHelper.safeApprove(token1_, address(uniswapPositionManager), amount1ToMint);
 
+        // Here, the parameters for miniting the position are set 
         INonfungiblePositionManager.MintParams memory params =
             INonfungiblePositionManager.MintParams({
                 token0: token0_,
@@ -185,12 +207,15 @@ contract LiquidityCustodian {
 
         // COMPLETED TODO: mint the liquidity position in the uniswap position manager using the params above
         // NOTE: this returns the tokenId of the minted NFT that represents the liquidity position
+        // Mint the NFT (returns its ID, liquidity amount, and actual token usage)
         (tokenId, liquidity, amount0, amount1) = uniswapPositionManager.mint(params);
 
         // COMPLETED TODO: create the deposit record for the minter/msg.sender
+        // Record this NFT as belonging to the user
         _createDeposit(msg.sender, tokenId);
 
-        // Remove allowance and refund in both assets.
+        // Remove allowance and refund in both assets
+        // If Uniswap didn’t need all of the user’s tokens, refund the leftovers
         if (amount0 < amount0ToMint) {
             TransferHelper.safeApprove(token0_, address(uniswapPositionManager), 0);
             uint256 refund0 = amount0ToMint - amount0;
@@ -217,6 +242,7 @@ contract LiquidityCustodian {
     }
 
     // COMPLETED TODO: complete the functions _deleteLiquidityToken
+    // Internal helper to remove a tokenId from the owner’s list
     function _deleteLiquidityToken(address ownerAddress, uint256 tokenId) private {
         uint256[] storage tokens = liquidityTokens[ownerAddress];
         for (uint256 i = 0; i < tokens.length; i++) {
--- packages/hardhat/contracts/MemeCoin.sol
+++ packages/hardhat/contracts/MemeCoin.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// Creating a simple ERC20 token contract for Meme Coin 
 contract MemeCoin is ERC20, Ownable{
 
     uint8 constant _decimals = 18;
@@ -12,10 +13,12 @@ contract MemeCoin is ERC20, Ownable{
     event PoolCreated(address tk, address pool, uint160 sqrtPriceX96);
 
     // COMPLETED TODO: create a _totalSupply of 1000 tokens which have 18 decimal places
+    // Total supply: 1000 tokens, adjusted for 18 decimals (so 1000 * 10^18 smallest units)
     uint256 private constant _totalSupply = 1000 * (10 ** uint256(_decimals));
 
     constructor() ERC20("Meme Coin", "MC") {
         // COMPLETED TODO: mint the _totalSupply of tokens to the owner
+        // Mint the entire supply and give it to the deployer (msg.sender). This means the deployer starts with all the tokens
         _mint(msg.sender, _totalSupply);
     }
 }
